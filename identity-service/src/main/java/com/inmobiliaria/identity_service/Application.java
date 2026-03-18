6a3b895f70cc5e1be271ddd7f4453ee55ca26507
Marino <marinobarroso2000@gmail.com>
2026-03-18 10:32:26 -0400

    Correcciones a microservicio de autenticacion con jwt

--- identity-service/src/main/java/com/inmobiliaria/identity_service/Application.java
+++ identity-service/src/main/java/com/inmobiliaria/identity_service/Application.java
@@ -2,8 +2,13 @@ package com.inmobiliaria.identity_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.inmobiliaria.identityservice.config.JwtProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
+@EnableFeignClients
 public class Application {
 
 	public static void main(String[] args) {
